fd8eb48ea2cac73352bf166220137a3e9f0c3b6d
frankperilli <f.perilli2@studenti.unisa.it>
2025-01-22 19:08:31 +0100

    Modifiche all'algoritmo per etichettare i nodi e disegnarlo

--- DisegnaGrafo.py
+++ DisegnaGrafo.py
@@ -2,7 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def draw_labeled_graph(labeled_graph):
+def draw_labeled_graph(labeled_graph, graph_name, labels):
     """
     Disegna un grafo etichettato. Ogni nodo e ogni spigolo sono visualizzati,
     e le etichette degli spigoli vengono mostrate sopra di essi.
@@ -11,18 +11,33 @@ def draw_labeled_graph(labeled_graph):
                           nella forma {v: {w: label}} dove v e w sono i nodi e label è l'etichetta dello spigolo (classe di equivalenza).
     """
     G = nx.Graph()
+    max_length = max(len(bin(label)[2:]) for label in labels.values())
+    label_dict = {v: bin(labels[v])[2:].zfill(max_length) for v in labels}
+    bit_difference = 0
     for v in labeled_graph:
-        for w, label in labeled_graph[v].items():
+        for w in labeled_graph[v]:
             if not G.has_edge(v, w):
-                G.add_edge(v, w, label=label)
-
+                v_label = label_dict[v]
+                w_label = label_dict[w]
+                for i in range(max_length - 1, -1, -1):
+                    if v_label[max_length - i - 1] != w_label[max_length - i - 1]:
+                        bit_difference = i
+                G.add_edge(v, w, label=bit_difference)
+                bit_difference = 0
     pos = nx.kamada_kawai_layout(G)
 
-    nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray', node_size=350, font_size=7,
-            font_weight='bold')
 
-    edge_labels = nx.get_edge_attributes(G, 'label')
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red', font_size=5, label_pos=0.5)
+    composed_labels = {}
+    for v in G:
+        composed_labels[v] = str(str(v) + "\n" + str(label_dict[v]))
+    plt.figure()
+    nx.draw(G, pos, with_labels=False, node_color='lightblue', edge_color='gray', node_size=850, font_size=10)
+    nx.draw_networkx_labels(G, pos, labels=composed_labels, font_size=7)
+    edge_labels = nx.get_edge_attributes(G, "label")
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red', font_size=7, label_pos=0.5)
 
-    plt.title("Grafo etichettato")
+    plt.draw()
+    plt.savefig("Grafi/" + graph_name)
     plt.show()
+
+    return max_length
--- PartialCube.py
+++ PartialCube.py
@@ -41,9 +41,13 @@ def PartialCubeEdgeLabeling(G):
     # - UF: union find data structure representing known edge equivalences
     # - CG: contracted graph at current stage of algorithm
     # - LL: limit on number of remaining available labels
+    # - vertex_labels save the labels relative to the node in the graph
+    # - components memorize the connected component at each step of the iteration
     UF = UnionFind()
     CG = {v: {w: (v, w) for w in G[v]} for v in G}
     NL = len(CG) - 1
+    vertex_labels = {}
+    components = {}
 
     # Initial sanity check: are there few enough edges?
     # Needed so that we don't try to use union-find on a dense
@@ -52,7 +56,7 @@ def PartialCubeEdgeLabeling(G):
     m = sum([len(CG[v]) for v in CG])
     if 1 << (m // n) > n:
         raise Medium.MediumError("graph has too many edges")
-
+    j = 0
     # Main contraction loop in place of the original algorithm's recursion
     while len(CG) > 1:
         if not isBipartite(CG):
@@ -72,12 +76,32 @@ def PartialCubeEdgeLabeling(G):
             bitvec[neighbor] = 1 << i
             neighbors[1 << i] = neighbor
             i += 1
-
         # Breadth first search to propagate bitvectors to the rest of the graph
         for LG in BFS.BreadthFirstLevels(CG, root):
             for v in LG:
                 for w in LG[v]:
                     bitvec[w] |= bitvec[v]
+        # for every vertex in G check which iteration we are executing
+        for v in G:
+            # at the first iteration save each label as saved in bitvec
+            if j == 0:
+                vertex_labels[v] = bitvec[v]
+            else:
+                # select the connected component associated with the vertex
+                node = components[0][v]
+                if j > 1:
+                    # starting from the second iteration use x to navigate components
+                    # and save in node the innermost component
+                    x = 1
+                    while x < j:
+                        node = components[x][node]
+                        x += 1
+                # put in new_label the label associated with the component
+                new_label = bitvec[node]
+                # put in old_label the label previously associate to the vertex
+                old_label = vertex_labels[v]
+                # concatenate the two labels and associate it to the vertex
+                vertex_labels[v] = concat_labels(old_label, new_label, deg)
 
         # Make graph of labeled edges and union them together
         labeled = {v: set() for v in CG}
@@ -93,7 +117,6 @@ def PartialCubeEdgeLabeling(G):
                 UF.union(CG[w][v], CG[neighbor][root])
                 labeled[v].add(w)
                 labeled[w].add(v)
-
         # Map vertices to components of labeled-edge graph
         component = {}
         compnum = 0
@@ -115,12 +138,12 @@ def PartialCubeEdgeLabeling(G):
                         UF.union(NG[vi][wi], CG[v][w])
                     else:
                         NG[vi][wi] = CG[v][w]
-
         CG = NG
-
+        components[j] = component
+        j += 1
     # Here with all edge equivalence classes represented by UF.
     # Turn them into a labeled graph and return it.
-    return {v: {w: UF[v, w] for w in G[v]} for v in G}
+    return {v: {w: UF[v, w] for w in G[v]} for v in G}, vertex_labels
 
 
 def MediumForPartialCube(G):
@@ -129,10 +152,10 @@ def MediumForPartialCube(G):
     Raises MediumError if G is not a partial cube.
     Uses the O(n^2) time algorithm of arxiv:0705.1025.
     """
-    L = PartialCubeEdgeLabeling(G)
+    L, labels = PartialCubeEdgeLabeling(G)
     M = Medium.LabeledGraphMedium(L)
     Medium.RoutingTable(M)  # verification step per arxiv:0705.1025
-    return M,L
+    return M,L,labels
 
 
 def PartialCubeLabeling(G):
@@ -143,39 +166,105 @@ def PartialCubeLabeling(G):
 def isPartialCube(G):
     """Test whether the given graph is a partial cube."""
     try:
-        M,L = MediumForPartialCube(G)
-        return True, L
+        M,L,labels = MediumForPartialCube(G)
+        return True, L, labels
     except Medium.MediumError:
-        return False, 0
+        return False, 0, 0
 
 
 # Perform some sanity checks if run standalone
+
+def concat_labels(old_label, new_label, pos):
+    # shift the old_label of a number of pos
+    # then do the OR between the two labels
+    concat_label = (old_label * pow(2, pos)) ^ new_label
+    return concat_label
 
 class PartialCubeTest(unittest.TestCase):
     # make medium from all five-bit numbers that have 2 or 3 bits lit
     twobits = [3, 5, 6, 9, 10, 12, 17, 18, 20, 24]
     threebits = [31 ^ x for x in twobits]
     M523 = Medium.BitvectorMedium(twobits + threebits, 5)
+    Q4_11_1000 = {1: [2, 3, 4], 2: [1, 7], 3: [1, 5], 4: [1, 5, 6], 5: [3, 4], 6: [4], 7: [2]}
+
+    Q5_11_1000 = {1: [2, 3], 2: [1, 4], 3: [1, 4, 5], 4: [2, 3, 6], 5: [3, 6, 7], 6: [4, 5, 8], 7: [5], 8: [6, 9, 10],
+                  9: [8], 10: [8]}
+
+    Q4_11_1010 = {1: [2, 3], 2: [1, 4], 3: [1, 4, 5], 4: [2, 3, 6, 7], 5: [3, 6], 6: [4, 5], 7: [4]}
+
+    Q4_101 = {1: [2, 4, 5, 9], 2: [1, 3, 6], 3: [2, 4, 7], 4: [1, 3, 8], 5: [1, 6, 10], 6: [2, 5, 7, 11], 7: [3, 6, 12],
+              8: [4, 9], 9: [1, 8, 10], 10: [5, 9, 11], 11: [6, 10, 12], 12: [7, 11]}
+
+    Q4_100 = {1: [2, 4], 2: [1, 3, 6, 10], 3: [2, 4, 7, 9], 4: [1, 3, 5], 5: [4, 7, 8], 6: [2, 7, 11], 7: [3, 5, 6, 12],
+              8: [5, 12], 9: [3, 10, 12], 10: [2, 9, 11], 11: [6, 10, 12], 12: [7, 8, 9, 11]}
 
     def testIsPartialCube(self):
         M = PartialCubeTest.M523
         G = Medium.StateTransitionGraph(M)
-        I,L = isPartialCube(G)
+        I,L,labels = isPartialCube(G)
         self.assertEqual(I, True)
-        print(L)
-        DisegnaGrafo.draw_labeled_graph(L)
+        if I:
+            iDim = str(DisegnaGrafo.draw_labeled_graph(L, "M523", labels))
+            print("The graph M523 is a partial cube and the iDim is:" + iDim)
+        else:
+            print("The graph M523 is not a partial cube")
 
     def testK4(self):
         G = {i: [j for j in range(4) if j != i] for i in range(4)}
-        I,L = isPartialCube(G)
+        I,L,labels = isPartialCube(G)
         self.assertEqual(I, False)
         print(L)
 
     def testK33(self):
         G = {0: [3, 4, 5], 1: [3, 4, 5], 2: [3, 4, 5], 3: [0, 1, 2], 4: [0, 1, 2], 5: [0, 1, 2]}
-        I,L = isPartialCube(G)
+        I,L,labels = isPartialCube(G)
         self.assertEqual(I, False)
         print(L)
+
+    def test_Q4_11_1000(self):
+        G = self.Q4_11_1000
+        I,L, labels = isPartialCube(G)
+        if I:
+            iDim = str(DisegnaGrafo.draw_labeled_graph(L, "Q4_11_1000", labels))
+            print("The graph Q4_11_1000 is a partial cube and the iDim is:" + iDim)
+        else:
+            print("The graph Q4_11_1000 is not a partial cube")
+
+    def test_Q5_11_1000(self):
+        G = self.Q5_11_1000
+        I,L,labels = isPartialCube(G)
+        if I:
+            iDim = str(DisegnaGrafo.draw_labeled_graph(L, "Q5_11_1000", labels))
+            print("The graph Q5_11_1000 is a partial cube and the iDim is:" + iDim)
+        else:
+            print("The graph Q5_11_1000 is not a partial cube")
+
+    def test_Q4_11_1010(self):
+        G = self.Q4_11_1010
+        I,L,labels = isPartialCube(G)
+        if I:
+            iDim = str(DisegnaGrafo.draw_labeled_graph(L, "Q4_11_1010", labels))
+            print("The graph Q4_11_1010 is a partial cube and the iDim is:" + iDim)
+        else:
+            print("The graph Q4_11_1010 is not a partial cube")
+
+    def test_Q4_101(self):
+        G = self.Q4_101
+        I,L,labels = isPartialCube(G)
+        if I:
+            iDim = str(DisegnaGrafo.draw_labeled_graph(L, "Q4_101",labels))
+            print("The graph Q4_101 is a partial cube and the iDim is:" + iDim)
+        else:
+            print("The graph Q4_101 is not a partial cube")
+
+    def test_Q4_100(self):
+        G = self.Q4_100
+        I,L,labels = isPartialCube(G)
+        if I:
+            iDim = str(DisegnaGrafo.draw_labeled_graph(L, "Q4_101", labels))
+            print("The graph Q4_100 is a partial cube and the iDim is:" + iDim)
+        else:
+            print("The graph Q4_100 is not a partial cube")
 
     def testMediumForPartialCube(self):
         """Check that we get an isomorphic medium via MediumForPartialCube."""
@@ -184,47 +273,47 @@ class PartialCubeTest(unittest.TestCase):
         # rather than equality of media.
         M = PartialCubeTest.M523
         G = Medium.StateTransitionGraph(M)
-        E,L = MediumForPartialCube(G)
+        E,L,labels = MediumForPartialCube(G)
         H = Medium.StateTransitionGraph(E)
         self.assertEqual(set(G), set(H))
         for v in G:
             self.assertEqual(set(G[v]), set(H[v]))
 
     def test6212(self):
-        """
-        A graph that can be labeled, but is not a partial cube.
-        Tests the code in LabeledGraphMedium that checks for the
-        existence of multiple equally labeled edges at a vertex.
-        """
-        n, a, b, c = 6, 2, 1, 2
-        G = {}
-        for i in range(n):
-            G[i, 0] = [(i, 1), ((i + b) % n, 3), ((i + c) % n, 3)]
-            G[i, 1] = [(i, 0), (i, 2), ((i - a) % n, 2)]
-            G[i, 2] = [(i, 1), (i, 3), ((i + a) % n, 1)]
-            G[i, 3] = [(i, 2), ((i - b) % n, 0), ((i - c) % n, 0)]
-        I, L = isPartialCube(G)
-        self.assertEqual(I, False)
-        print(L)
+       """
+       A graph that can be labeled, but is not a partial cube.
+       Tests the code in LabeledGraphMedium that checks for the
+       existence of multiple equally labeled edges at a vertex.
+       """
+       n, a, b, c = 6, 2, 1, 2
+       G = {}
+       for i in range(n):
+           G[i, 0] = [(i, 1), ((i + b) % n, 3), ((i + c) % n, 3)]
+           G[i, 1] = [(i, 0), (i, 2), ((i - a) % n, 2)]
+           G[i, 2] = [(i, 1), (i, 3), ((i + a) % n, 1)]
+           G[i, 3] = [(i, 2), ((i - b) % n, 0), ((i - c) % n, 0)]
+       I, L, labels = isPartialCube(G)
+       self.assertEqual(I, False)
+       print(L)
 
-    def test61150(self):
-        """
-        Another graph that can be labeled, but is not a partial cube.
-        Tests the code in RoutingTable that makes sure that only tokens
-        in a single direction belong to the initial list of active tokens.
-        """
-        G = {}
-        n, a, b, c, d = 6, 1, 1, 5, 0
-        for i in range(n):
-            G[i, 0] = [(i, 1), ((i + a) % n, 5), ((i + b) % n, 3)]
-            G[i, 1] = [(i, 0), (i, 2), ((i - d) % n, 4)]
-            G[i, 2] = [(i, 1), (i, 3), ((i + c) % n, 5)]
-            G[i, 3] = [(i, 2), (i, 4), ((i - b) % n, 0)]
-            G[i, 4] = [(i, 3), (i, 5), ((i + d) % n, 1)]
-            G[i, 5] = [(i, 4), ((i - a) % n, 0), ((i - c) % n, 2)]
-        I, L = isPartialCube(G)
-        self.assertEqual(I, False)
-        print(L)
+    # def test61150(self):
+    #    """
+    #    Another graph that can be labeled, but is not a partial cube.
+    #    Tests the code in RoutingTable that makes sure that only tokens
+    #    in a single direction belong to the initial list of active tokens.
+    #    """
+    #    G = {}
+    #    n, a, b, c, d = 6, 1, 1, 5, 0
+    #    for i in range(n):
+    #        G[i, 0] = [(i, 1), ((i + a) % n, 5), ((i + b) % n, 3)]
+    #        G[i, 1] = [(i, 0), (i, 2), ((i - d) % n, 4)]
+    #        G[i, 2] = [(i, 1), (i, 3), ((i + c) % n, 5)]
+    #        G[i, 3] = [(i, 2), (i, 4), ((i - b) % n, 0)]
+    #        G[i, 4] = [(i, 3), (i, 5), ((i + d) % n, 1)]
+    #        G[i, 5] = [(i, 4), ((i - a) % n, 0), ((i - c) % n, 2)]
+    #    I, L, labels = isPartialCube(G)
+    #    self.assertEqual(I, False)
+    #    print(L)
 
 
 if __name__ == "__main__":
